62ee18966dc4557836535ff7da4ead26cd60212b
Vamsi Krishna <20816971+petlurivamsi@users.noreply.github.com>
2024-12-24 11:36:09 +0530

    Intersection observer basic example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const observer = new IntersectionObserver(function (entries) {
+      entries.forEach((entry) => {
+        console.log("::entry in frame ", entry);
+        entry.target.style.backgroundColor = "red";
+      });
+    }, {});
+    const target = document.getElementById("observerEx");
+    observer.observe(target);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">1</div>
+      <div className="box">2</div>
+      <div className="box" id="observerEx">
+        3
+      </div>
+      <div className="box">4</div>
+      <div className="box">5</div>
     </div>
   );
 }
